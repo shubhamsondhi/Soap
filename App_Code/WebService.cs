2145ee8b40a37f6aedcd5b7b23dfae31bf177b25
shubhamsondhi <shubhamsondhi@gmail.com>
2017-04-29 21:21:42 -0400

    Add files via upload

---
+++ App_Code/WebService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Net.Mail;
+using System.Net;
+/// <summary>
+/// Summary description for WebService
+/// </summary>
+/// 
+[WebService(Namespace="http://microsoft.com/webservices/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+[System.Web.Script.Services.ScriptService]
+
+public class WebService : System.Web.Services.WebService {
+
+    public WebService () {
+             //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public void HelloWorld(string name, string surname, string email, string phone, string message1)
+    {
+
+        string names = name;
+        string server = "smtp.gmail.com";
+        int port = 587;
+        string to = "sh.sondhi@gmail.com";
+        string from = email;
+        string subject = "Feedback From"+ name+" "+surname+" email is: "+email;
+        string body = @message1;
+        MailMessage message = new MailMessage(from, to, subject, body);
+        SmtpClient client = new SmtpClient(server, port);
+        // Credentials are necessary if the server requires the client 
+        // to authenticate before it will send e-mail on the client's behalf.
+        client.Credentials = new System.Net.NetworkCredential("sh.sondhi@gmail.com", "Shrewd6622");
+        client.EnableSsl = true;
+        //new System.Net.NetworkCredential("username", "password");
+
+
+        try
+        {
+            client.Send(message);
+        }
+        catch (Exception ex)
+        {
+            HttpContext.Current.Response.Redirect("../contact.html?id=0");
+
+        }
+        HttpContext.Current.Response.Redirect("../contact.html?id=1");
+
+       
+    }
+    
+}
---
+++ js/contact.js
@@ -0,0 +1,29 @@
+$(function () {
+
+    $('#contact-form').validator();
+    
+});
+
+$(document).ready(function () {
+    var id = GetParameterValues('id');
+        if (id == 0) {
+        document.getElementById("message").innerHTML = "Sorry, please send message again";
+        } else if (id == null) {
+            document.getElementById("message").innerHTML = "";
+        }
+
+        else {
+            document.getElementById("message").style.color = "green";
+        document.getElementById("message").innerHTML = "Thank you For Your Feedback";
+
+    }
+    function GetParameterValues(param) {
+        var url = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for (var i = 0; i < url.length; i++) {
+            var urlparam = url[i].split('=');
+            if (urlparam[0] == param) {
+                return urlparam[1];
+            }
+        }
+    }
+});
